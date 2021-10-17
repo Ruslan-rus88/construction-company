67b5b81182950a2d4076ab104c12c486a9906c18
Ruslan Khoussein <ruslan.khoussein@MacBook-Pro-Admin.local>
2021-10-18 01:13:23 +0300

    added navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CareerComponent } from './pages/career/career.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ServicesComponent } from './pages/services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: 'career', component: CareerComponent},
+  {path: 'contacts', component: ContactsComponent},
+  { path: '**', redirectTo: 'home'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
