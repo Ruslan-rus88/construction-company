740e23cd88d057816352aa8f5ac72acf89200d98
Ruslan Khoussein <ruslan.khoussein@MacBook-Pro-Admin.local>
2021-12-10 02:08:47 +0200

    added projects

--- src/app/pages/gallery/gallery-projects.ts
+++ src/app/pages/gallery/gallery-projects.ts
@@ -18,5 +18,80 @@ export const projectsData = [
             { url: 'assets/images/gallery/project1/13.jpg', alt: '' },
             { url: 'assets/images/gallery/project1/14.jpg', alt: '' }
         ]
-    }
+    },
+    {
+        project_name: 'Innenausbau _ Badezimmer mit großformatigen Fliesen',
+        location: '58453 Witten',
+        images: [
+            { url: 'assets/images/gallery/project2/1.jpg', alt: '' },
+            { url: 'assets/images/gallery/project2/2.jpg', alt: '' },
+            { url: 'assets/images/gallery/project2/3.jpg', alt: '' },
+            { url: 'assets/images/gallery/project2/4.jpg', alt: '' },
+            { url: 'assets/images/gallery/project2/5.jpg', alt: '' },
+        ]
+    },
+    {
+        project_name: 'Badezimmer mit großformatigen Fliesen',
+        location: '44795 Bochum',
+        images: [
+            { url: 'assets/images/gallery/project3/1.jpg', alt: '' },
+            { url: 'assets/images/gallery/project3/2.jpg', alt: '' },
+            { url: 'assets/images/gallery/project3/3.jpg', alt: '' },
+            { url: 'assets/images/gallery/project3/4.jpg', alt: '' },
+            { url: 'assets/images/gallery/project3/5.jpg', alt: '' },
+            { url: 'assets/images/gallery/project3/6.jpg', alt: '' },
+        ]
+    },
+    {
+        project_name: 'Badrenovierung',
+        location: '44869 Bochum',
+        images: [
+            { url: 'assets/images/gallery/project4/1.jpg', alt: '' },
+            { url: 'assets/images/gallery/project4/2.jpg', alt: '' },
+            { url: 'assets/images/gallery/project4/3.jpg', alt: '' },
+        ]
+    },
+    {
+        project_name: 'Laden',
+        location: '40237 Düsseldorf',
+        images: [
+            { url: 'assets/images/gallery/project5/1.jpg', alt: '' },
+            { url: 'assets/images/gallery/project5/2.jpg', alt: '' },
+            { url: 'assets/images/gallery/project5/3.jpg', alt: '' },
+            { url: 'assets/images/gallery/project5/4.jpg', alt: '' },
+            { url: 'assets/images/gallery/project5/5.jpg', alt: '' },
+            { url: 'assets/images/gallery/project5/6.jpg', alt: '' },
+            { url: 'assets/images/gallery/project5/7.jpg', alt: '' },
+            { url: 'assets/images/gallery/project5/8.jpg', alt: '' },
+            { url: 'assets/images/gallery/project5/9.jpg', alt: '' },
+            { url: 'assets/images/gallery/project5/10.jpg', alt: '' },
+            { url: 'assets/images/gallery/project5/11.jpg', alt: '' },
+            { url: 'assets/images/gallery/project5/12.jpg', alt: '' },
+            { url: 'assets/images/gallery/project5/13.jpg', alt: '' },
+        ]
+    },
+    {
+        project_name: 'Kreativer Innenausbau',
+        location: '45665 Recklinghausen',
+        images: [
+            { url: 'assets/images/gallery/project6/1.jpg', alt: '' },
+            { url: 'assets/images/gallery/project6/2.jpg', alt: '' },
+            { url: 'assets/images/gallery/project6/3.jpg', alt: '' },
+            { url: 'assets/images/gallery/project6/4.jpg', alt: '' },
+            { url: 'assets/images/gallery/project6/5.jpg', alt: '' },
+            { url: 'assets/images/gallery/project6/6.jpg', alt: '' },
+            { url: 'assets/images/gallery/project6/7.jpg', alt: '' },
+        ]
+    },
+    {
+        project_name: 'Wohnung',
+        location: '45131 Essen',
+        images: [
+            { url: 'assets/images/gallery/project7/1.jpg', alt: '' },
+            { url: 'assets/images/gallery/project7/2.jpg', alt: '' },
+            { url: 'assets/images/gallery/project7/3.jpg', alt: '' },
+            { url: 'assets/images/gallery/project7/4.jpg', alt: '' },
+            { url: 'assets/images/gallery/project7/5.jpg', alt: '' },
+        ]
+    },
 ]
