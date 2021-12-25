126a1a480942bd2418d7be0d7569435f3df75e32
Ruslan Khoussein <ruslan.khoussein@MacBook-Pro-Admin.local>
2021-12-25 02:09:30 +0200

    added photo to career page

--- src/app/pages/career/career.component.ts
+++ src/app/pages/career/career.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./career.component.scss']
 })
 export class CareerComponent implements OnInit {
+  public cardsArray: number[] = Array(25).fill(1)
+  private interval: any;
+  public index: number = 0
+  public showTitle: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.interval = window.setInterval(() => {
+      document.querySelectorAll('.career__img-card')[this.index].classList.add('career__img-card--rotate')
+      this.index++;
+      console.log(this.index);
+      if (this.index === 25) {
+        this.showTitle = true;
+        window.clearInterval(this.interval);
+      }
+     }, 100);
   }
 
 }
